01b9eed16c46fab7f66b28caa114a6fec727716a
Chris Laprun <metacosm@gmail.com>
2011-11-09 22:29:59 +0100

    - Started messing with UI.

---
+++ ShadowrunAssistant/SRAConstantsSR4.h
@@ -0,0 +1,13 @@
+//
+//  SRAConstantsSR4.h
+//  ShadowrunAssistant
+//
+//  Created by Christophe Laprun on 11/9/11.
+//  Copyright (c) 2011 JBoss, a division of Red Hat. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SRAConstantsSR4 : NSObject
+extern NSString const *ATTR_BODY;
+@end
---
+++ ShadowrunAssistant/SRAConstantsSR4.m
@@ -0,0 +1,13 @@
+//
+//  SRAConstantsSR4.m
+//  ShadowrunAssistant
+//
+//  Created by Christophe Laprun on 11/9/11.
+//  Copyright (c) 2011 JBoss, a division of Red Hat. All rights reserved.
+//
+
+#import "SRAConstantsSR4.h"
+
+@implementation SRAConstantsSR4
+
+@end
