0b01a5e7a741df83ab0b8c084b0fba1794e1922a
Chris Laprun <metacosm@gmail.com>
2011-11-12 11:31:28 +0100

    - Added initialization of constants.

--- ShadowrunAssistant/SRAConstants.m
+++ ShadowrunAssistant/SRAConstants.m
@@ -8,6 +8,16 @@
 
 #import "SRAConstants.h"
 
+NSString const *ATTR_AGILITY = @"agility";
+NSString const *ATTR_BODY = @"body";
+NSString const *ATTR_REACTION = @"reaction";
+NSString const *ATTR_STRENGTH = @"strength";
+NSString const *ATTR_CHARISMA = @"charisma";
+NSString const *ATTR_INTUITION = @"intuition";
+NSString const *ATTR_LOGIC = @"logic";
+NSString const *ATTR_WILLPOWER = @"willpower";
+NSString const *ATTR_EDGE = @"edge";
+
 @implementation SRAConstants
 
 @end
